d51b8d60a8e206482ad5673887ca219256f177d5
Soloman Garcia <soloman.garcia@gmail.com>
2022-09-07 12:20:49 -0600

    style chart

--- js/app.js
+++ js/app.js
@@ -37,42 +37,46 @@ function getNumber(str) {
   return Number(str.replace(/[^0-9\.-]+/g, ""));
 }
 
-const ctx = document.getElementById("myChart").getContext("2d");
-const myChart = new Chart(ctx, {
-  type: "bar",
+let ctx = document.getElementById("myChart").getContext("2d");
+let myChart = new Chart(ctx, {
+  type: "doughnut",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: labels,
     datasets: [
       {
         label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+        data: [
+          monthlyPrincipalInterest,
+          monthlyPropertyTaxes,
+          monthlyHomeInsurance,
+          monthlyHOA,
         ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
         borderWidth: 1,
       },
     ],
   },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
 });
+
+myChart.options.animation = false;
+
+let i;
+let inputTexts = document.getElementsByClassName("form-group__textInput");
+for (let i = 0; i < inputTexts.length; i++) {
+  inputTexts[i].addEventListener("input", updateInputsState);
+}
+
+function updateInputsState(event) {
+  let name = event.target.name;
+  let value = event.target.value;
+  if (name == "price") {
+    value = getNumber(value);
+  }
+  state = {
+    ...state,
+    [name]: value
+  }
+}
 
 console.log(state);
